86dae7f130c5638a7f015fb72ab83b278de8338e
srohinireddy <srohinireddy@gmail.com>
2020-10-16 00:16:22 +0530

    all folders added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CoreWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CoreWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ViewResult> GetRealData()
+        {
+            RealData data = new RealData();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://api.covid19api.com/summary"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    data = JsonConvert.DeserializeObject<RealData>(apiResponse);
+                }
+            }
+            return View(data);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/RealData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApp.Models
+{
+    public class RealData
+    {
+        public string Message { get; set; }
+        public GlobalData Global { get; set; }
+        public List<CountryInfo> Countries { get; set; }
+    }
+    public struct GlobalData
+    {
+        public double NewConfirmed;
+        public double TotalConfirmed;
+        public double NewDeaths;
+        public double TotalDeaths;
+        public double NewRecovered;
+        public double TotalRecovered;
+    }
+    public struct CountryInfo
+    {
+        public string Country;
+        public string CountryCode;
+        public string Slug;
+        public double NewConfirmed;
+        public double TotalConfirmed;
+        public double NewDeaths;
+        public double TotalDeaths;
+        public double NewRecovered;
+        public double TotalRecovered;
+        public string Date;
+    }
+}
